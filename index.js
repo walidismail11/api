fa095a4e4e5a3a15c51318a17a535dff75d5fa82
alijoumaa <alijoumaa@users.noreply.github.com>
2022-01-04 22:22:00 +0200

    first

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require("express")
-const app = express(); 
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const { MongoClient } = require("mongodb");
-const userRoute = require("./rootes/user")
-const authRoute = require("./rootes/auth")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./rootes/user");
+const authRoute = require("./rootes/auth");
 const productRoute = require("./rootes/product");
 const cartRoute = require("./rootes/cart");
 const orderRoute = require("./rootes/order");
 const stripeRoute = require("./rootes/stripe");
-const uri = process.env.MONGODB_URI;
 const cors = require("cors");
-const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-dotenv.config()
+dotenv.config();
 
-client.connect();
+mongoose
+  .connect("mongodb+srv://quickshop:quickshop154@cluster0.j4onu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(cors());
-app.use(express.json())
-
+app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
@@ -27,7 +28,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT, () => {
-    console.log("Backend server is runnning")
-})
-
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
